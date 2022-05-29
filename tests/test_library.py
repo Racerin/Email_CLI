0c377f5994ed98922ec21e5d175e3f4d8e9381ba
Racerin <drsbaird@yahoo.com>
2022-05-29 19:33:14 -0400

    test: Add test files

---
+++ tests/test_library.py
@@ -0,0 +1,9 @@
+import unittest
+
+from library import *
+
+class TestEmail(unittest.TestCase):
+    """ Test behaviors and methods in the Email class. """
+    def test_send_email(self):
+        pass
+    
